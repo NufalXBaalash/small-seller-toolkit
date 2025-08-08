6a958920f142279b3a4aa2be0d56bfddb0f6a76f
NufalXBaalash <nufalbaalash@gmail.com>
2025-08-08 20:14:29 +0300

    Terms and Privacy

---
+++ app/terms/page.tsx
@@ -0,0 +1,9 @@
+export default function TermsPage() {
+    return (
+      <div className="p-4">
+        <h1 className="text-xl font-bold">Terms and Conditions</h1>
+        <p>This is a placeholder for Terms and Conditions.</p>
+      </div>
+    );
+  }
+  
